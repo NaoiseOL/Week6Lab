9d10ddf22339691ae7ee84ff40b726282c71340a
NaoiseOL <naoiseol123@gmail.com>
2024-10-28 17:26:07 +0000

    Added ServiceController class and integrated into the Get and Post Methods

--- src/main/java/ie/atu/week6/ProductController.java
+++ src/main/java/ie/atu/week6/ProductController.java
@@ -2,22 +2,29 @@ package ie.atu.week6;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private ServiceController myService;
+
+    public ProductController(ServiceController myservice) {
+        this.myService = myservice;
+    }
 
     private List<Product> list = new ArrayList<>();
 
     @GetMapping
     public List<Product> getProducts() {
+        list = myService.getProducts();
         return list;
     }
 
     @PostMapping
     public List<Product> newProduct(@RequestBody Product product) {
-        list.add(product);
+        list = myService.addProduct(product);
         return list;
     }
 
